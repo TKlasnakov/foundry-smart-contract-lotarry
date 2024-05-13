09d7446ed307138d2f285fe4e2de8aa5e25cffaf
Todor Klasnakov <todor.klasnakov@paysafe.com>
2024-05-13 08:38:29 +0300

    add VRF in the contract

--- src/Raffle.sol
+++ src/Raffle.sol
@@ -11,13 +11,36 @@ pragma solidity ^0.8.18;
 contract Raffle {
     error Raffle__NotEnouhEtherSent();
 
+    uint256 private constant REQUEST_CONFRIMATIONS = 3;
+    uint256 private constant NUMBER_OF_WORDS = 1;
+
     uint256 private immutable i_entranceFee;
+    uint256 private immutable i_interval;
+    uint64 private immutable i_subscriptionId;
+    address private immutable i_vrfCoordinator;
+    bytes32 private immutable i_gasLine;
+    uint32 private immutable i_callbackGasLimit;
+
+    uint256 private s_lastTimstamp;
     address payable[] private s_players;
 
     event EnteredRaffle(address indexed player);
 
-    constructor(uint256 entranceFee) {
+    constructor(
+        uint256 entranceFee,
+        uint256 interval,
+        address vrfCoordinator,
+        bytes32 gasLine /** keyHash */,
+        uint64 subscriptionId,
+        uint32 callbackGasLimit
+    ) {
         i_entranceFee = entranceFee;
+        i_interval = interval;
+        i_vrfCoordinator = vrfCoordinator;
+        i_gasLine = gasLine;
+        i_subscriptionId = subscriptionId;
+        i_callbackGasLimit = callbackGasLimit;
+        s_lastTimstamp = block.timestamp;
     }
 
     function enterRuffles() external payable {
@@ -28,7 +51,19 @@ contract Raffle {
         emit EnteredRaffle({player: msg.sender});
     }
 
-    function pickWinner() public {}
+    function pickWinner() public {
+        if ((block.timestamp - s_lastTimstamp) < i_interval) {
+            revert();
+        }
+
+        uint256 requestId = i_vrfCoordinator.requestRandomWords(
+            i_gasLine,
+            i_subscriptionId,
+            REQUEST_CONFRIMATIONS,
+            i_callbackGasLimit,
+            NUMBER_OF_WORDS
+        );
+    }
 
     function getEntranceFee() external view returns (uint256) {
         return i_entranceFee;
